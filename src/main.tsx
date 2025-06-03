5b66a842290784cbfccfecd87c0942078fc0f35b
sedrac-bitkabir <sedraclucas@bitkabir.com>
2025-06-03 22:44:30 +0100

    test navigation in netifly

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
-import App from './App.tsx'
+import App from './App.tsx';
+import LoginPage from './page/LoginPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/about" element={<LoginPage />} />
+    </Routes>
+  </BrowserRouter>
 )
