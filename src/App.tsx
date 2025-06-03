d3e9f87b8845981fb87efce323a89b048f79ef79
sedrac-bitkabir <sedraclucas@bitkabir.com>
2025-06-03 23:16:47 +0100

    test navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { Button } from "@/components/ui/button"
+import { NavLink } from "react-router-dom"
 
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
+      <NavLink
+        to="/login"
+        className={({ isActive }) =>
+          isActive ? "active" : ""
+        }
+      >
+        Login
+      </NavLink>
       <Button>Click me</Button>
     </div>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,23 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
-import App from './App.tsx';
+import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import LoginPage from './page/LoginPage.tsx';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+    {
+    path: "/login",
+    element: <LoginPage/>,
+  },
+]);
+
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/about" element={<LoginPage />} />
-    </Routes>
-  </BrowserRouter>
+  <StrictMode>
+     <RouterProvider router={router} />
+  </StrictMode>,
 )
